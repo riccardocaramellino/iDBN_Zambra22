e5aedfb8a1d36004f7b468a0582187a24aded8a2
LorenzoTausani <83968253+LorenzoTausani@users.noreply.github.com>
2023-05-10 19:55:40 +0200

    Update Study_generativity.py

--- src/dbn-utls/Study_generativity.py
+++ src/dbn-utls/Study_generativity.py
@@ -10,6 +10,13 @@ import scipy
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import math
+import VGG_MNIST
+import methods
+from VGG_MNIST import *
+from methods import *
+from google.colab import files
+from itertools import combinations
+
 
 
 
@@ -372,7 +379,133 @@ class Intersection_analysis_ZAMBRA:
       lbl_bias_freqV = digit_digit_common_elements_count_biasing.view(100)/torch.sum(digit_digit_common_elements_count_biasing.view(100))
 
       return digit_digit_common_elements_count_biasing
+    def generate_chimera_lbl_biasing(self,VGG_cl, elements_of_interest = [8,2], temperature=1, nr_of_examples = 1000, plot=0, entropy_correction=0):
+      b_vec =torch.zeros(nr_of_examples,2000) #this 2000 seems the layersize hardcoded
+      if not(elements_of_interest =='rand'):
+        dictionary_key = str(elements_of_interest[0])+','+str(elements_of_interest[1])
+        b_vec[:,self.result_dict_biasing[dictionary_key].long()]=1
+
+      else: #write 'rand' in elements of interest
+        for i in range(nr_of_examples):
+          n1 = random.randint(0, 9)
+          n2 = random.randint(0, 9)
+          dictionary_key = str(n1)+','+str(n2)
+          b_vec[i,self.result_dict_biasing[dictionary_key].long()]=1
+
+      b_vec = torch.transpose(b_vec,0,1)
+      #b_vec = torch.unsqueeze(b_vec,0)
+      d = generate_from_hidden_ZAMBRA(self.model, b_vec, nr_gen_steps=self.nr_steps)
+      
+      d = Classifier_accuracy(d, VGG_cl, self.model, plot=plot, Thresholding_entropy=entropy_correction)
+      df_average,df_sem, Transition_matrix_rowNorm = classification_metrics(d,self.model, Plot=plot, Ian=1)
+      
+      if nr_of_examples < 16:
+        Plot_example_generated(d, self.model ,row_step = 10, dS=20, custom_steps = True, Show_classification = False)
+
+      
+      return d, df_average,df_sem, Transition_matrix_rowNorm
     
+def Chimeras_nr_visited_states_ZAMBRA(model, VGG_cl, Ian =[], topk=149, apprx=1,plot=1,compute_new=1, nr_sample_generated =100, entropy_correction=[], lS=20):
+    def save_mat_xlsx(my_array, filename='my_res.xlsx'):
+        # create a pandas dataframe from the numpy array
+        my_dataframe = pd.DataFrame(my_array)
+
+        # save the dataframe as an excel file
+        my_dataframe.to_excel(filename, index=False)
+        # download the file
+        files.download(filename)
+
+    n_digits = model.Num_classes
+    if Ian!=[]:
+      fN='Visited_digits_k' + str(Ian.top_k_Hidden)+'.xlsx'
+      fNerr='Visited_digits_error_k' + str(Ian.top_k_Hidden)+'.xlsx'
+      fN_NDST='Nondigit_stateTime_k' + str(Ian.top_k_Hidden)+'.xlsx'
+      fNerr_NDST='Nondigit_stateTime_error_k' + str(Ian.top_k_Hidden)+'.xlsx'
+    else:
+      fN='Visited_digits_Lbiasing_k' + str(topk)+'.xlsx'
+      fNerr='Visited_digits_Lbiasing_error_k' + str(topk)+'.xlsx'
+      fN_NDST='Nondigit_stateTime_Lbiasing_k' + str(topk)+'.xlsx'
+      fNerr_NDST='Nondigit_stateTime_Lbiasing_error_k' + str(topk)+'.xlsx'
+
+    if compute_new==1:
+      #both
+      Vis_states_mat = np.zeros((n_digits, n_digits))
+      Vis_states_err = np.zeros((n_digits, n_digits))
+      Non_digit_mat  = np.zeros((n_digits, n_digits))
+      Non_digit_err  = np.zeros((n_digits, n_digits))
+
+      if Ian!=[]:
+        for row in range(n_digits):
+          for col in range(row,n_digits):
+            d, df_average,df_sem, Transition_matrix_rowNorm = Ian.generate_chimera_lbl_biasing(VGG_cl,elements_of_interest = [row,col], nr_of_examples = nr_sample_generated, temperature = 1, plot=0, entropy_correction= entropy_correction)
+            Vis_states_mat[row,col]=df_average.Nr_visited_states[0]
+            Vis_states_err[row,col]=df_sem.Nr_visited_states[0]
+            Non_digit_mat[row,col] = df_average['Non-digit'][0]
+            Non_digit_err[row,col] = df_sem['Non-digit'][0]
+      else:
+        numbers = list(range(n_digits))
+        combinations_of_two = list(combinations(numbers, 2))
+
+        for idx, combination in enumerate(combinations_of_two):
+          gen_hidden = label_biasing_ZAMBRA(model, on_digits=  list(combination), topk = topk)
+          gen_hidden_rep = gen_hidden.repeat(1,nr_sample_generated)
+          d = generate_from_hidden_ZAMBRA(model, gen_hidden_rep , nr_gen_steps=100)
+          d = Classifier_accuracy(d, VGG_cl,model, Thresholding_entropy=entropy_correction, labels=[], Batch_sz= 100, plot=0, dS=30, l_sz=3)
+          df_average,df_sem, Transition_matrix_rowNorm = classification_metrics(d,model,Plot=0,dS=50,Ian=1)
+          Vis_states_mat[combination[0],combination[1]]=df_average.Nr_visited_states[0]
+          Vis_states_err[combination[0],combination[1]]=df_sem.Nr_visited_states[0]
+          Non_digit_mat[combination[0],combination[1]] = df_average['Non-digit'][0]
+          Non_digit_err[combination[0],combination[1]] = df_sem['Non-digit'][0]
+
+
+      save_mat_xlsx(Vis_states_mat, filename=fN)
+      save_mat_xlsx(Vis_states_err, filename=fNerr)
+      save_mat_xlsx(Non_digit_mat, filename=fN_NDST)
+      save_mat_xlsx(Non_digit_err, filename=fNerr_NDST)
+
+    else: #load already computed Vis_states_mat
+      Vis_states_mat = pd.read_excel(fN)
+      Non_digit_mat = pd.read_excel(fN_NDST)
+      # Convert the DataFrame to a NumPy array
+      Vis_states_mat = Vis_states_mat.values
+      Non_digit_mat = Non_digit_mat.values
+
+      Vis_states_err = pd.read_excel(fNerr)
+      Non_digit_err = pd.read_excel(fNerr_NDST)
+      # Convert the DataFrame to a NumPy array
+      Vis_states_err = Vis_states_err.values
+      Non_digit_err = Non_digit_err.values
+
+    if plot==1:
+
+      Vis_states_mat = Vis_states_mat.round(apprx)
+      Vis_states_err = Vis_states_err.round(apprx)
+
+      plt.figure(figsize=(15, 15))
+      mask = np.triu(np.ones_like(Vis_states_mat),k=+1) # k=+1 per rimuovere la diagonale
+      # Set the lower triangle to NaN
+      Vis_states_mat = np.where(mask==0, np.nan, Vis_states_mat)
+      Vis_states_mat = Vis_states_mat.T
+      #ax = sns.heatmap(Vis_states_mat, linewidth=0.5, annot=False,square=True, cbar=False)
+      ax = sns.heatmap(Vis_states_mat, linewidth=0.5, annot=True, annot_kws={"size": lS},square=True,cbar_kws={"shrink": .82}, fmt='.1f', cmap='jet')
+
+      #ax.set_xticklabels(T_mat_labels)
+      ax.tick_params(axis='both', labelsize=lS)
+
+      plt.xlabel('Digit', fontsize = 25) # x-axis label with fontsize 15
+      plt.ylabel('Digit', fontsize = 25) # y-axis label with fontsize 15
+      #cbar = plt.gcf().colorbar(ax.collections[0], location='left', shrink=0.82)
+      cbar = ax.collections[0].colorbar
+      cbar.ax.tick_params(labelsize=lS)
+      plt.show()
+
+    return Vis_states_mat, Vis_states_err,Non_digit_mat,Non_digit_err
+
+
+
+
+
+
 
 def Perc_H_act(model, sample_labels, gen_data_dictionary=[], dS = 50, l_sz = 5, layer_of_interest=2):
 
