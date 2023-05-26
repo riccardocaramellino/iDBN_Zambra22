c2d6f0dd608cf05013b26b2719d8d71c78e2c01a
LorenzoTausani <83968253+LorenzoTausani@users.noreply.github.com>
2023-05-26 19:12:07 +0200

    Update Study_generativity.py

--- src/dbn-utls/Study_generativity.py
+++ src/dbn-utls/Study_generativity.py
@@ -550,3 +550,92 @@ def Perc_H_act(model, sample_labels, gen_data_dictionary=[], dS = 50, l_sz = 5, 
 
     axis.set_ylim([0,100])
     return Mean_storing, Sem_storing
+
+def Plot_example_generated(input_dict, model,row_step = 10, dS=20, custom_steps = True, Show_classification = False, not_random_idxs = True):
+    side_img = int(np.sqrt(model.rbm_layers[0].Nin))
+    Generated_samples=input_dict['vis_states']
+    nr_steps = Generated_samples.shape[2]
+
+    if Show_classification ==True:
+      Classifications = input_dict['Cl_pred_matrix']
+    
+    if custom_steps == True:
+      steps=[2,3,4,5,10,25,50,100]
+      rows=len(steps)
+    else:
+      steps = range(row_step,nr_steps+1,row_step) #controlla che funzioni
+      rows = math.floor(nr_steps/row_step) 
+
+    cols = Generated_samples.shape[0]
+
+    if cols>10:
+      figure, axis = plt.subplots(rows+1,10, figsize=(25*(10/10),2.5*(1+rows)))
+    elif cols>1:
+      figure, axis = plt.subplots(rows+1,cols, figsize=(25*(cols/10),2.5*(1+rows)))
+    else:
+      figure, axis = plt.subplots(rows+1,cols+1, figsize=(25*(cols/10),2.5*(1+rows)))
+
+    if cols >= 10:
+      if not_random_idxs ==True:
+        random_numbers = range(10)
+      else:
+        random_numbers = random.sample(range(cols), 10) # 10 random samples are selected
+    else:
+      random_numbers = random.sample(range(cols), cols) # 10 random samples are selected
+
+
+    c=0
+    for sample_idx in random_numbers: #per ogni sample selezionato
+
+        # plotto la ricostruzione dopo uno step
+
+        reconstructed_img= Generated_samples[sample_idx,:,0] #estraggo la prima immagine ricostruita per il particolare esempio (lbl può essere un nome un po fuorviante)
+        reconstructed_img = reconstructed_img.view((side_img,side_img)).cpu() #ridimensiono l'immagine e muovo su CPU
+        axis[0, c].tick_params(left = False, right = False , labelleft = False ,
+            labelbottom = False, bottom = False)
+        axis[0, c].imshow(reconstructed_img , cmap = 'gray')
+        if Show_classification==True:
+          axis[0, c].set_title("Class {}".format(Classifications[sample_idx,0]), fontsize=dS)
+        if c==0:
+          ylabel = axis[0, c].set_ylabel("Step {}".format(1), fontsize=dS,rotation=0, labelpad=70)
+
+        axis[0, c].set_xticklabels([])
+        axis[0, c].set_yticklabels([])
+        axis[0, c].set_aspect('equal')
+
+        #for idx,step in enumerate(range(row_step,nr_steps+1,row_step)): # idx = riga dove plotterò, step è il recostruction step che ci plotto
+        for idx,step in enumerate(steps): # idx = riga dove plotterò, step è il recostruction step che ci plotto
+            idx = idx+1 #sempre +1 perchè c'è sempre 1 step reconstruction
+
+            #plotto la ricostruzione
+
+            reconstructed_img= Generated_samples[sample_idx,:,step-1] #step-1 perchè 0 è la prima ricostruzione
+            reconstructed_img = reconstructed_img.view((side_img,side_img)).cpu()
+            axis[idx, c].tick_params(left = False, right = False , labelleft = False ,
+            labelbottom = False, bottom = False)
+            axis[idx, c].imshow(reconstructed_img , cmap = 'gray')
+            if Show_classification==True:
+              axis[idx, c].set_title("Class {}".format(Classifications[sample_idx,step-1]), fontsize=dS)
+            #axis[idx, lbl].set_title("Step {}".format(step) , fontsize=dS)
+            if c==0:
+              ylabel = axis[idx, c].set_ylabel("Step {}".format(step), fontsize=dS, rotation=0, labelpad=70)
+              
+
+
+            axis[idx, c].set_xticklabels([])
+            axis[idx, c].set_yticklabels([])
+            axis[idx, c].set_aspect('equal')
+
+        c=c+1
+
+    #aggiusto gli spazi tra le immagini
+    plt.subplots_adjust(left=0.1, 
+                        bottom=0.1,  
+                        right=0.9,  
+                        top=0.9,  
+                        wspace=0.4,  
+                        hspace=0.2) 
+    
+    #plt.savefig("Reconstuct_plot.jpg") #il salvataggio è disabilitato
+
+    plt.show()
