6f8c951a64c5fc268861620902569cc9a5ea2d31
LorenzoTausani <83968253+LorenzoTausani@users.noreply.github.com>
2023-10-12 15:21:17 +0200

    Update dbns.py

--- src/dbn-utls/dbns.py
+++ src/dbn-utls/dbns.py
@@ -35,7 +35,7 @@ class DBN(torch.nn.Module):
                          layer_id = 1,
                          init_scheme = init_scheme,
                          dataset_id = dataset_id),
-                rbms.RBM(500, 2000, epochs, #ORIGINALLY 500,1000
+                rbms.RBM(500, 1000, epochs, #ORIGINALLY 500,1000. 2000 in Zambra 2022
                          layer_id = 2, 
                          init_scheme = init_scheme, 
                          dataset_id = dataset_id)
