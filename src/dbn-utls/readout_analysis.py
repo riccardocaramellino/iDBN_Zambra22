497ec1f285e9ed69e387da16d127e63a401e07bb
LorenzoTausani <83968253+LorenzoTausani@users.noreply.github.com>
2023-10-23 14:13:45 +0200

    modifiche per avere generazione durante training. Probabilmente da riaggiustare

--- src/dbn-utls/readout_analysis.py
+++ src/dbn-utls/readout_analysis.py
@@ -61,7 +61,6 @@ def readout_V_to_Hlast(dbn,train_dataset,test_dataset, DEVICE='cuda', existing_c
       #end WITH
       print(rbm_idx)
       #if rbm_idx==2:
-      print('eccomi')
       upper = int(_Xtrain.shape[0]*(4/5))
       
       if len(existing_classifier_list) == 0:
@@ -95,7 +94,23 @@ def get_relative_freq(valore, hist, bin_edges,numero_bin=30):
     else:
         return 0.0  # Il valore è al di fuori dei bin
 
-def get_retraining_data(MNIST_train_dataset, dbn=[], classifier=[], n_steps_generation = 10, ds_type = 'EMNIST', half_MNIST_gen=True, Type_gen = 'chimeras', selection_gen = False, correction_type = 'frequency'):
+def load_existing_retrainDS(trainfile_path, testfile_path, nr_batches_retraining):
+
+  train_dataset = dict(np.load(trainfile_path))
+  test_dataset = dict(np.load(testfile_path))
+  # Convert the numpy arrays to torch tensors
+  for key in train_dataset:
+      train_dataset[key] = torch.from_numpy(train_dataset[key])
+      test_dataset[key]= torch.from_numpy(test_dataset[key])
+
+  train_data_retraining_ds = train_dataset['data'][:nr_batches_retraining,:,:]
+  train_labels_retraining_ds = train_dataset['labels'][:nr_batches_retraining,:,:]
+  train_dataset_retraining_ds = {'data': train_data_retraining_ds, 'labels': train_labels_retraining_ds}
+  test_dataset_retraining_ds = test_dataset
+  return train_dataset_retraining_ds, test_dataset_retraining_ds
+  
+
+def get_retraining_data(MNIST_train_dataset, train_dataset_retraining_ds = {}, dbn=[], classifier=[], n_steps_generation = 10, ds_type = 'EMNIST', half_MNIST_gen=True, Type_gen = 'chimeras', selection_gen = False, correction_type = 'frequency'):
   #Type_gen = 'chimeras'/'lbl_bias'/'mix'
   #NOTA: il labelling dell'EMNIST by class ha 62 labels: le cifre (0-9), lettere MAUSCOLE (10-36), lettere MINUSCOLE(38-62)
   #20,000 uppercase letters from the first 10 EMNIST classes.
@@ -111,37 +126,47 @@ def get_retraining_data(MNIST_train_dataset, dbn=[], classifier=[], n_steps_gene
         vectors.append(torch.mean(batch,axis = 1))
     avg_pixels_active_TrainMNIST = torch.cat(vectors) #This is the distribution of avg pixels active in the MNIST train dataset
 
-
+  
   def decrease_labels_by_10(data): 
     image, label = data
     label -= 10  # Sottrai 10 da ciascuna etichetta
     return image, label
   #load  EMNIST byclass data
-  transform =transforms.Compose([transforms.ToTensor()])
-  if ds_type == 'EMNIST':
-    data_train_retraining_ds = datasets.EMNIST('../data', train=True,split = 'byclass', download=True, transform=transform)
-    data_test_retraining_ds = datasets.EMNIST('../data', train=False,split = 'byclass', download=True, transform=transform)
-    target_classes = list(range(10, 20)) #i.e. the first 10 capital letter classes
-    data_train_retraining_ds = [item for item in data_train_retraining_ds if item[1] in target_classes]
-    data_test_retraining_ds = [item for item in data_test_retraining_ds if item[1] in target_classes]
-    #i relabel data from 10-19 to 0-9
-    data_train_retraining_ds = [decrease_labels_by_10(item) for item in data_train_retraining_ds]
-    data_test_retraining_ds = [decrease_labels_by_10(item) for item in data_test_retraining_ds]
-  elif ds_type == 'fMNIST':
-    data_train_retraining_ds = datasets.FashionMNIST('../data', train=True, download=True, transform=transform)
-    data_test_retraining_ds = datasets.FashionMNIST('../data', train=False, download=True, transform=transform)
+  if not(bool(train_dataset_retraining_ds)):
+    Zambra_folder_drive = '/content/gdrive/My Drive/ZAMBRA_DBN/'
+    test_filename = 'test_dataset_'+ds_type+'.npz'
+    train_filename = 'train_dataset_'+ds_type+'.npz'
+    trainfile_path= os.path.join(Zambra_folder_drive,'dataset_dicts',train_filename)
+    testfile_path = os.path.join(Zambra_folder_drive,'dataset_dicts',test_filename) 
+    if os.path.exists(trainfile_path):
+      train_dataset_retraining_ds, test_dataset_retraining_ds = load_existing_retrainDS(trainfile_path, testfile_path, nr_batches_retraining)
+    else:
+      transform =transforms.Compose([transforms.ToTensor()])
+      if ds_type == 'EMNIST':
+          data_train_retraining_ds = datasets.EMNIST('../data', train=True,split = 'byclass', download=True, transform=transform)
+          data_test_retraining_ds = datasets.EMNIST('../data', train=False,split = 'byclass', download=True, transform=transform)
+          target_classes = list(range(10, 20)) #i.e. the first 10 capital letter classes
+          data_train_retraining_ds = [item for item in data_train_retraining_ds if item[1] in target_classes]
+          data_test_retraining_ds = [item for item in data_test_retraining_ds if item[1] in target_classes]
+          #i relabel data from 10-19 to 0-9
+          data_train_retraining_ds = [decrease_labels_by_10(item) for item in data_train_retraining_ds]
+          data_test_retraining_ds = [decrease_labels_by_10(item) for item in data_test_retraining_ds]
+      elif ds_type == 'fMNIST':
+          data_train_retraining_ds = datasets.FashionMNIST('../data', train=True, download=True, transform=transform)
+          data_test_retraining_ds = datasets.FashionMNIST('../data', train=False, download=True, transform=transform)
 
+      train_data_retraining_ds, train_labels_retraining_ds = data_and_labels(data_train_retraining_ds, BATCH_SIZE=128,NUM_FEAT=np.int32(28*28),DATASET_ID='MNIST',n_cols_labels=1)
+      test_data_retraining_ds, test_labels_retraining_ds = data_and_labels(data_test_retraining_ds, BATCH_SIZE=128,NUM_FEAT=np.int32(28*28),DATASET_ID='MNIST',n_cols_labels=1)
+    
+      #i select just 20000 examples (19968 per l'esattezza)
+      train_data_retraining_ds = train_data_retraining_ds[:nr_batches_retraining,:,:]
+      train_labels_retraining_ds = train_labels_retraining_ds[:nr_batches_retraining,:,:]
+      train_dataset_retraining_ds = {'data': train_data_retraining_ds, 'labels': train_labels_retraining_ds}
+      test_dataset_retraining_ds = {'data': test_data_retraining_ds, 'labels': test_labels_retraining_ds}
 
-  train_data_retraining_ds, train_labels_retraining_ds = data_and_labels(data_train_retraining_ds, BATCH_SIZE=128,NUM_FEAT=np.int32(28*28),DATASET_ID='MNIST',n_cols_labels=1)
-  test_data_retraining_ds, test_labels_retraining_ds = data_and_labels(data_test_retraining_ds, BATCH_SIZE=128,NUM_FEAT=np.int32(28*28),DATASET_ID='MNIST',n_cols_labels=1)
-  
-  #i select just 20000 examples (19968 per l'esattezza)
-  train_data_retraining_ds = train_data_retraining_ds[:nr_batches_retraining,:,:]
-  train_labels_retraining_ds = train_labels_retraining_ds[:nr_batches_retraining,:,:]
-  train_dataset_retraining_ds = {'data': train_data_retraining_ds, 'labels': train_labels_retraining_ds}
-  test_dataset_retraining_ds = {'data': test_data_retraining_ds, 'labels': test_labels_retraining_ds}
-  if dbn==[]:
-     return train_dataset_retraining_ds,test_dataset_retraining_ds
+    if dbn==[]:
+        return train_dataset_retraining_ds,test_dataset_retraining_ds
+    
   if not(half_MNIST_gen):
      half_MNIST = MNIST_train_dataset['data'][:half_batches,:,:].to('cuda')
   else:
@@ -223,8 +248,15 @@ def get_retraining_data(MNIST_train_dataset, dbn=[], classifier=[], n_steps_gene
   permuted_indices = torch.randperm(nr_batches_retraining)
   # Use the permutation to shuffle the examples of the dataset
   mix_retraining_ds_MNIST = mix_retraining_ds_MNIST[permuted_indices]
+  try:
+    # Prova ad accedere alla variabile 'my_variable'.
+    print(test_dataset_retraining_ds)
+    return train_dataset_retraining_ds,test_dataset_retraining_ds,mix_retraining_ds_MNIST
+  except NameError:
+    # Gestisci l'eccezione se la variabile non esiste.
+    print("La variabile 'my_variable' non esiste.")
+    return mix_retraining_ds_MNIST
   
-  return train_dataset_retraining_ds,test_dataset_retraining_ds,mix_retraining_ds_MNIST
 
 def get_ridge_classifiers(MNIST_Train_DS, MNIST_Test_DS, Force_relearning = True):
   Zambra_folder_drive = '/content/gdrive/My Drive/ZAMBRA_DBN/'
@@ -269,7 +301,7 @@ def get_ridge_classifiers(MNIST_Train_DS, MNIST_Test_DS, Force_relearning = True
 
 
 
-def relearning(retrain_ds_type = 'EMNIST', mixing_type =[], n_steps_generation=10, selection_gen = False, correction_type = 'frequency'):
+def relearning(retrain_ds_type = 'EMNIST', mixing_type =[], n_steps_generation=10, new_retrain_data = False, selection_gen = False, correction_type = 'frequency'):
     DEVICE='cuda'
     dbn,MNISTtrain_ds, MNISTtest_ds,classifier= tool_loader_ZAMBRA(DEVICE, only_data = False,Load_DBN_yn = 1)
     mixing_type_options = ['origMNIST', 'lbl_bias', 'chimeras','[]']
@@ -291,7 +323,7 @@ def relearning(retrain_ds_type = 'EMNIST', mixing_type =[], n_steps_generation=1
     else:
        half_MNIST_gen_option = True
 
-    Retrain_ds,Retrain_test_ds,mix_retrain_ds = get_retraining_data(MNISTtrain_ds,dbn, classifier,n_steps_generation = n_steps_generation,  ds_type = retrain_ds_type, half_MNIST_gen=half_MNIST_gen_option, Type_gen = mixing_type, selection_gen = selection_gen, correction_type = correction_type)
+    Retrain_ds,Retrain_test_ds,mix_retrain_ds = get_retraining_data(MNISTtrain_ds,{},dbn, classifier,n_steps_generation = n_steps_generation,  ds_type = retrain_ds_type, half_MNIST_gen=half_MNIST_gen_option, Type_gen = mixing_type, selection_gen = selection_gen, correction_type = correction_type)
     MNIST_classifier_list, _ = get_ridge_classifiers(MNISTtrain_ds, MNISTtest_ds,Force_relearning = False)
 
     
@@ -303,7 +335,14 @@ def relearning(retrain_ds_type = 'EMNIST', mixing_type =[], n_steps_generation=1
         LPARAMS = json.load(filestream)
     with open(os.path.join(Zambra_folder_drive, 'cparams.json'), 'r') as filestream:
         CPARAMS = json.load(filestream)
-    LPARAMS['EPOCHS']=5 #i will checj t
+
+    if new_retrain_data == True:
+       LPARAMS['EPOCHS']=1
+       inner_loop_epochs = 5
+    else:
+       LPARAMS['EPOCHS']=5
+       inner_loop_epochs = 1
+
     READOUT = CPARAMS['READOUT']
     NUM_DISCR = CPARAMS['NUM_DISCR']
 
@@ -323,8 +362,15 @@ def relearning(retrain_ds_type = 'EMNIST', mixing_type =[], n_steps_generation=1
     readout_acc_V_LETTERS,_ = readout_V_to_Hlast(dbn,Retrain_ds,Retrain_test_ds)
     readout_acc_Seq_RETRAINING_DS[0,:] = readout_acc_V_LETTERS
 
+
+
     for iteration in range(nr_iter_training):
-        dbn.train(Xtrain, Xtest, Ytrain, Ytest, LPARAMS, readout = READOUT, num_discr = NUM_DISCR)
+        for ep in range(inner_loop_epochs):
+          if new_retrain_data == True:
+            mix_retrain_ds = get_retraining_data(MNISTtrain_ds,Retrain_ds,dbn, classifier,n_steps_generation = n_steps_generation,  ds_type = retrain_ds_type, half_MNIST_gen=half_MNIST_gen_option, Type_gen = mixing_type, selection_gen = selection_gen, correction_type = correction_type)
+            Xtrain = mix_retrain_ds.to(DEVICE)
+            print(ep)
+          dbn.train(Xtrain, Xtest, Ytrain, Ytest, LPARAMS, readout = READOUT, num_discr = NUM_DISCR)
         readout_acc_V_DIGITS,_ = readout_V_to_Hlast(dbn,train_dataset,test_dataset,existing_classifier_list = MNIST_classifier_list)
         readout_acc_Seq_DIGITS[iteration+1,:] = readout_acc_V_DIGITS
 
@@ -343,3 +389,4 @@ def relearning(retrain_ds_type = 'EMNIST', mixing_type =[], n_steps_generation=1
     Readout_last_layer_MNIST = df_readout_MNIST.values[:, len(dbn.rbm_layers)]
 
     return Readout_last_layer_MNIST, Readout_last_layer_RETRAINING_DS
+
